19e828b55a345eb8b2617003a497c6b9ac043391
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-05-27 11:14:38 -0600

    Added token recieving

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const $userSignUpForm = document.querySelector("#user-signup")
+const $userLoginForm = document.querySelector("#user-login")
 
 $userSignUpForm.addEventListener("submit", e => {
     e.preventDefault()
@@ -16,4 +17,27 @@ $userSignUpForm.addEventListener("submit", e => {
         body: JSON.stringify({username, password})
     })
     $userSignUpForm.reset()
+})
+
+$userLoginForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const formData = new FormData(event.target)
+
+    const username = formData.get("username")
+    const password = formData.get("password")
+
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username, password})
+    })
+    .then(response => response.json())
+    .then(response => {
+        const {token} = response
+        alert(token)
+    })
+    $userLoginForm.reset()
 })
