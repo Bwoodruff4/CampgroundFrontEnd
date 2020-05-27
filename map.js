50490c371381785d40131fa13b55f710d9bf20ee
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-05-27 16:27:59 -0600

    Cleaned up styling for header

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
 let map;
+
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 39.7392, lng: -104.9903},
         zoom: 8
     });
 }
